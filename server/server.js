00a38ac4d8b87c02fc3058ba82e0ca0258fe9d41
Shashank reddy Yata <shashankreddyyata@shashanks-mbp.lan>
2018-09-26 16:32:39 -0500

    Added GET API

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ var app = express();
 
 app.use(bodyParser.json());
 
+//POST REST API
 app.post('/todos',(req,res) =>{
   var todo = new Todo({
     text : req.body.text
@@ -19,6 +20,15 @@ app.post('/todos',(req,res) =>{
   },(e) => {
     res.status(400).send(e);
   });
+});
+
+//GET REST API
+app.get('/todos',(req,res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  },(e) => {
+    res.status(400).send(e);
+  })
 });
 
 app.listen(3000, () =>{
