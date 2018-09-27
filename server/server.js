2746f0b61e3eea84dee98997a20320fb063d67d9
Shashank reddy Yata <shashankreddyyata@shashanks-mbp.lan>
2018-09-26 23:45:48 -0500

    Remove added

--- server/server.js
+++ server/server.js
@@ -42,7 +42,7 @@ app.get('/todos/:id',(req,res) => {
    }
    Todo.findById(id).then((todo) =>{
       if (!todo){
-        return res.status(404).send;
+        return res.status(404).send();
       }
       res.send({todo});
    }).catch((e) => {
@@ -50,6 +50,22 @@ app.get('/todos/:id',(req,res) => {
    });
 })
 
+//REMOVE REST API
+app.delete('/todos/:id',(req,res) => {
+  let id =req.params.id;
+
+  if(!ObjectId.isValid(id)) {
+    return res.status(404).send();
+  }
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if(!todo){
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
+})
 app.listen(port, () =>{
   console.log('Server is started on port',port);
 });
